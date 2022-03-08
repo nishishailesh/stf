e2c8f4ff08c319cf842382cd22091cc862780914
Your Name <you@example.com>
2022-03-08 23:05:43 +0530

    modified:   update_stf_to_git_pass.sh

--- update_stf_to_git_pass.sh
+++ update_stf_to_git_pass.sh
@@ -6,5 +6,5 @@ read password
 mysqldump   -h127.0.0.1 -u$user -p$password hostel > stf_blank.sql 
 git add *
 git commit -a
-git push https://github.com/nishishailesh/stf master
+git push https://github.com/nishishailesh/stf main
 
